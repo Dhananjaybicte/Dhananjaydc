cc7e31bc6579151e2e55f505483e5b96f7310d03
Dhananjaybicte <dhananjaybicte@gmail.com>
2024-10-22 13:47:57 +0545

    hello

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Components/Main';
+import './Components/Style.css';
+import Card from './Components/Card';
+import Model from './Components/Model';
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
